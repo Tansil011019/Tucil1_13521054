00e42d1beeadaaa40ba7726b5c42cbc6ec4b33d8
Tansil011019 <16521295@std.stei.itb.ac.id>
2023-01-21 00:36:55 +0700

    feat: final-clean

--- src/24games.c
+++ src/24games.c
@@ -3,17 +3,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+// boolean declaration 
 #define boolean unsigned char
 #define true 1
 #define false 0
 
-#define TABLE_SIZE 100
-
-struct HashTable {
-    int key;
-    int value;
-};
-
+// input data type and decalaration
 typedef struct{
     double array[4];
     int length;
@@ -21,6 +16,7 @@ typedef struct{
 
 data input_data;
 
+// input data combination data type and declaration
 typedef struct{
     double all_combination_data[1000][4];
     int length;
@@ -28,12 +24,14 @@ typedef struct{
 
 combination_data combi_data;
 
+// operator declaration
 enum operator {PLUS = 1, MINUS = 2, DIVIDE = 3, MUL = 4};
 
 int operator_arr[4] = {PLUS, MINUS, DIVIDE, MUL};
 
 char operator_char[4] = {'+', '-', '/', '*'};
 
+// operator combination data type and declaration
 typedef struct{
     int all_combination_data[1000][3];
     int length;
@@ -41,6 +39,7 @@ typedef struct{
 
 combination_op combi_op;
 
+// result combination data type and declaration
 typedef struct{
     char all_combination_data[1000][1000];
     int length;
@@ -48,69 +47,41 @@ typedef struct{
 
 combination_res result_storage;
 
+// stopwatch declaration
 clock_t start_time, end_time;
 
-struct HashTable ht[TABLE_SIZE];
-
-int hash(int key) {
-    return key % TABLE_SIZE;
-}
-
-void insert(int key, int value) {
-    int index = hash(key);
-    while (ht[index].key != -1) {
-        index = (index + 1) % TABLE_SIZE;
-    }
-    ht[index].key = key;
-    ht[index].value = value;
-}
-
-int search(int key) {
-    int index = hash(key);
-    int counter = 0;
-    while (ht[index].key != key && counter++ < TABLE_SIZE) {
-        index = (index + 1) % TABLE_SIZE;
-    }
-    return ht[index].value;
-}
-
+// result of an expression between two number
 double result_count(double first_num, int op, double second_num){
     if(op == 1){
-        // printf("%d + %d\n", first_num, second_num);
         return first_num + second_num;
     }else if(op == 2){
-        // printf("%d - %d\n", first_num, second_num);
         return first_num - second_num;
     }else if(op == 3){
         if(second_num != 0){
-            // printf("%d / %d\n", first_num, second_num);
             return first_num / second_num;  
         }else{
-            return -28561;
+            return -28561; // if the divisor is 0, then it will be minus 13^4 (the greatest amount of the card possible) so that it is impossible to result 24
         }
     }else if(op == 4){
-        // printf("%d * %d\n", first_num, second_num);
         return first_num * second_num;
     }
 }
 
+// find all operator combination (take 3 from 4)
 void operator_permute(){
     for(int i= 0; i< 4; i++){
         for(int j= 0; j< 4; j++){
             for(int k= 0; k< 4; k++){
                 combi_op.all_combination_data[combi_op.length][0] = operator_arr[i];
-                // printf("This is operator = %d\n", operator_arr[i]);
-                // printf("This is combi = %d\n", combi_op.all_combination_data[combi_op.length][0]);
                 combi_op.all_combination_data[combi_op.length][1] = operator_arr[j];
-                // printf("This is combi = %d\n", combi_op.all_combination_data[combi_op.length][1]);
                 combi_op.all_combination_data[combi_op.length][2] = operator_arr[k];
-                // printf("This is combi = %d\n", combi_op.all_combination_data[combi_op.length][2]);
                 combi_op.length++;
             }
         }
     }
 }
 
+// store the result in result data type that had been declared before
 void convert_operation_and_store(double * num, int op[], int type){
     char str[100];
     if(type == 5){
@@ -125,27 +96,30 @@ void convert_operation_and_store(double * num, int op[], int type){
         sprintf(str, "((%0.f %c %0.f) %c %0.f) %c %0.f", num[0], operator_char[(op[0])-1], num[1], operator_char[(op[1])-1], num[2], operator_char[(op[2])-1], num[3]);
     }
     boolean duplicate = false;
+    // find if there is duplicate value
     for(int i= 0; i< result_storage.length && !duplicate; i++){
         if(strcmp(result_storage.all_combination_data[i], str) == 0){
             duplicate = true;
         }
     }
+    // if there is no duplicate value than it is valid to be stored
     if(!duplicate){
         strcpy(result_storage.all_combination_data[result_storage.length], str);
         result_storage.length++;
     }
 }
     
-
+// game progression place
 void games_operator(){
     for(int i= 0; i< combi_op.length; i++){
         for(int j= 0; j< combi_data.length; j++){
+            // every bracket combination
             double combination_bracket1 = result_count(result_count(result_count(combi_data.all_combination_data[j][0], combi_op.all_combination_data[i][0], combi_data.all_combination_data[j][1]), combi_op.all_combination_data[i][1], combi_data.all_combination_data[j][2]), combi_op.all_combination_data[i][2], combi_data.all_combination_data[j][3]);
             double combination_bracket2 = result_count(result_count(combi_data.all_combination_data[j][0], combi_op.all_combination_data[i][0], result_count(combi_data.all_combination_data[j][1], combi_op.all_combination_data[i][1], combi_data.all_combination_data[j][2])), combi_op.all_combination_data[i][2], combi_data.all_combination_data[j][3]);
             double combination_bracket3 = result_count(combi_data.all_combination_data[j][0], combi_op.all_combination_data[i][0], result_count(result_count(combi_data.all_combination_data[j][1], combi_op.all_combination_data[i][1], combi_data.all_combination_data[j][2]), combi_op.all_combination_data[i][2], combi_data.all_combination_data[j][3]));
             double combination_bracket4 = result_count(combi_data.all_combination_data[j][0], combi_op.all_combination_data[i][0], result_count(combi_data.all_combination_data[j][1], combi_op.all_combination_data[i][1], result_count(combi_data.all_combination_data[j][2], combi_op.all_combination_data[i][2], combi_data.all_combination_data[j][3])));
             double combination_bracket5 = result_count(result_count(combi_data.all_combination_data[j][0], combi_op.all_combination_data[i][0], combi_data.all_combination_data[j][1]), combi_op.all_combination_data[i][1], result_count(combi_data.all_combination_data[j][2], combi_op.all_combination_data[i][2], combi_data.all_combination_data[j][3]));
-            // printf("This is result %d %d %d %d %d\n", combination_bracket1, combination_bracket2, combination_bracket3, combination_bracket4, combination_bracket5);
+            // if the bracket combination result is 24
             if(combination_bracket1 == 24){
                 convert_operation_and_store(combi_data.all_combination_data[j], combi_op.all_combination_data[i], 1);
             }
@@ -165,20 +139,20 @@ void games_operator(){
     }
 }
 
+// swapping two double variable
 void swap(double *a, double *b) {
     double temp = *a;
     *a = *b;
     *b = temp;
 }
 
+// find all combination of input data using recursion
 void insert_combination_to_matrix(double list[], int start, int end) {
     if (start == end) {
         for (int i = 0; i <= end; i++) {
             combi_data.all_combination_data[combi_data.length][i] = list[i];
-            // printf("%f ", combi_data.all_combination_data[combi_data.length][i]);
         }
         combi_data.length++;
-        // printf("\n");
     }
     else {
         for (int i = start; i <= end; i++) {
@@ -189,20 +163,20 @@ void insert_combination_to_matrix(double list[], int start, int end) {
     }
 }
 
+// get combination of input data and operator
 void get_all_combination(){
     insert_combination_to_matrix(input_data.array, 0, 3);
-    // printf("--------------------------------\n");
     operator_permute();
-    // printf("Im here\n");
-    // printf("This is one of the combi = %d\n", combi_op.all_combination_data[0][0]);
 }
 
+// the begining of the process where we find all the combination and process it
 void brute_force(){
     start_time = clock();
     get_all_combination();
     games_operator();
 }
 
+// input random data which is validated as instruction which is poker card
 void input_random(){
     srand(time(NULL));
     printf("Your random input is = ");
@@ -223,6 +197,7 @@ void input_random(){
     printf("\n");
 }
 
+// read user input and convert to double
 void read_input(){
     char input[5];
     double temp;
@@ -248,9 +223,11 @@ void read_input(){
         input_data.array[input_data.length] = temp;
         input_data.length++;
     }
+    // counter for input in one line
     fgets(input, 10000, stdin);
 }
 
+// user input beginning and instruction
 void input_user(){
     printf(
         "Rules:\n"
@@ -261,6 +238,7 @@ void input_user(){
     read_input();
 }
 
+// user input choice with validator wheter user want to random input or input their own
 void input_choice(){
     result_storage.length = 0;
     combi_data.length = 0;
@@ -278,6 +256,7 @@ void input_choice(){
     }
 }
 
+// output part with some instruction wheter want to display on terminal or file and validator
 void output_choice();
 
 void progress_output(int choice){
@@ -325,6 +304,7 @@ void output_choice(){
     progress_output(choice_output);
 }
 
+// restart progress
 void home();
 
 void restart_choice();
@@ -352,6 +332,7 @@ void restart_choice(){
     restart_progress(restart);
 }
 
+// home page
 void home(){
     printf(
 "________    _____                                        \n"                            
@@ -376,10 +357,8 @@ void home(){
     restart_choice();
 }
 
+// main
 int main(){
     system("cls");
     home();
-    // for(int i= 0; i<input_data.length; i++){
-    //     printf("%f\n", input_data.array[i]);
-    // }
 }
